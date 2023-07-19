3f8601cdb38fd85bc5c1a058737d7265e7f4318b
bernardo brotelli <b.brotelle@gmail.com>
2023-07-19 15:16:09 -0300

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do |i|
+  Article.create(title: "Ola mundo #{i}", content: "Wikinum e suas variedades #{i}")
+end
